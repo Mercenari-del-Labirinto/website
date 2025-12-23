2d8b0530c55a577a425c49303ded760624ae29ed
Giovanni Toraldo <me@gionn.net>
2025-12-23 22:29:05 +0100

    update site description

--- src/consts.ts
+++ src/consts.ts
@@ -4,5 +4,5 @@
 export const SITE_TITLE = "Mercenari del Labirinto";
 export const SITE_SHORT_TITLE = "Mercenari";
 export const SITE_DESCRIPTION =
-  "Sito ufficiale dei Mercenari del Labirinto A.S.D.";
+  "Sito ufficiale dei Mercenari del Labirinto - un gruppo di rievocazione storica italiano dedito alla pratica dell'arceria storica europea.";
 export const SITE_LOCALE = "it-IT";
